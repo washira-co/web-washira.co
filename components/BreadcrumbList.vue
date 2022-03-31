b672a04acb913c03fdfc6c3a323603de066445b7
Eiichi <8284623+eiichi-worker@users.noreply.github.com>
2022-03-31 16:50:49 +0900

    プライバシーポリシーを追加

---
+++ components/BreadcrumbList.vue
@@ -0,0 +1,55 @@
+<template>
+  <ol class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+    <li
+      v-for="item in list"
+      itemprop="itemListElement"
+      itemscope
+      itemtype="https://schema.org/ListItem"
+    >
+      <a itemprop="item" :href="item.url">
+        <span itemprop="name">{{ item.name }}</span>
+      </a>
+      <meta itemprop="position" :content="item.position" />
+    </li>
+  </ol>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["list"],
+};
+</script>
+
+<style lang="scss">
+.breadcrumb {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.breadcrumb li {
+  display: inline; /*横に並ぶように*/
+  list-style: none;
+  font-weight: bold; /*太字*/
+}
+
+.breadcrumb li:after {
+  /* >を表示*/
+  content: ">";
+  padding: 0 0.2em;
+  color: #555;
+}
+
+.breadcrumb li:last-child:after {
+  content: "";
+}
+
+.breadcrumb li a {
+  text-decoration: none;
+  color: #52b5ee; /*色*/
+}
+
+.breadcrumb li a:hover {
+  text-decoration: underline;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div>
-      <h1>合同会社わしら<small>Wasira LLC</small></h1>
-      <img src="/img/logo/wasilogo01.png" width="400" />
+      <!-- <h1>合同会社わしら<small>Wasira LLC</small></h1> -->
+      <img src="/img/logo/wasilogo01.png" width="400" alt="合同会社わしら|Wasira LLC" />
     </div>
 
     <div>
-      <h2>合同会社わしらとは<small>About</small></h2>
+      <h2>合同会社わしらとは <small>[About]</small></h2>
       <h3>『自分』と『自分の周り』をちょっと幸せにし続けます</h3>
       <p>
         多様な価値観の渦巻く時代になり、多くの人の同様の価値観というのは薄れてきました。<br />
@@ -19,35 +19,45 @@
     </div>
 
     <div>
-      <h2>事業内容<small>Works</small></h2>
+      <h2>大事にする価値観 <small>[Values]</small></h2>
+      <ul>
+        <li>HRT(謙虚・尊重・信頼)な心でお互い仕事をする</li>
+        <li>素直な心で仕事をする</li>
+        <li>透明性の高い仕事をする</li>
+        <li>ムリ・ムダ・ムラの無い仕事をする</li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>事業内容 <small>[Businesses]</small></h2>
       <ul>
         <li>猫を幸せにする事業</li>
-        <li>IT部門委託事業</li>
         <li>
-          →
-          <a href="https://techmesse.com/tech/1278" target="_blank"
-            >情報システム部門チームのアウトソーシング | TechMesse</a
-          >
+          IT部門委託事業
+          <ul>
+            <li>
+              <a href="https://techmesse.com/tech/1278" target="_blank"
+                >情報システム部門チームのアウトソーシング | TechMesse</a
+              >
+            </li>
+          </ul>
         </li>
         <li>IT部門内製化支援事業</li>
       </ul>
     </div>
 
     <div>
-      <h2>製品/サービス<small>Service</small></h2>
+      <h2>製品/サービス <small>[Services]</small></h2>
       <ul>
         <li>moreneko.club ※開発中</li>
       </ul>
     </div>
 
     <div>
-      <h2>正社員的なメンバー<small>Members</small></h2>
-      <div></div>
-      <h2>パート的なメンバー<small>Members</small></h2>
-      <div></div>
-      <h2>社員/出資者、経営者的なメンバー<small>Members</small></h2>
+      <h2>メンバー <small>[Members]</small></h2>
       <div>
-        <h3>藤本 永一 <small>代表社員 / プロダクトエンジニア</small></h3>
+        <h3>藤本 永一 <small>(Eiichi Fujimoto)</small></h3>
+        <p>代表社員 / プロダクトエンジニア</p>
         <img src="/img/members/001-001.png" width="150" />
         <p>
           FAエンジニアからプログラマを目指して、専門学校で学び直し、デジタルマーケティング会社にWEBエンジニアとして再就職、社内システム、受託でのサイトやシステム作成、自社のSaaS開発を経てエンジニアチームのマネジメントも担当。経験を活かし、世界を変えたい人や、楽しく働ける環境を作りたい人へエンジニアリングでお手伝いするため独立。
@@ -61,15 +71,22 @@
           </li>
         </ul>
       </div>
-      <h2>サポート的なメンバー<small>Members</small></h2>
-      <div></div>
+
+      <h2>パートナー <small>[Partners]</small></h2>
+      <div>
+        <ul>
+          <li>WEBデザイナー</li>
+          <li>WEBエンジニア</li>
+          <li>メンター/コーチング</li>
+        </ul>
+      </div>
     </div>
 
     <div>
-      <h2>SNS<small>sns</small></h2>
+      <h2>SNS <small>[SNS]</small></h2>
       <ul>
         <li>
-          <a href="https://twitter.com/washira_co" target="_blank">twitter</a>
+          <a href="https://twitter.com/washira_co" target="_blank">Twitter</a>
         </li>
         <li>
           <a href="https://www.linkedin.com/company/washira" target="_blank">linkedin</a>
@@ -80,13 +97,13 @@
           >
         </li>
         <li>
-          <a href="https://github.com/washira-co" target="_blank">github</a>
+          <a href="https://github.com/washira-co" target="_blank">GitHub</a>
         </li>
       </ul>
     </div>
 
     <div>
-      <h2>法人情報<small>Company</small></h2>
+      <h2>法人情報 <small>[Company]</small></h2>
       <table>
         <tr>
           <th>会社名</th>
@@ -119,7 +136,7 @@
               href="https://tayori.com/form/c787ef402de80671fcd7ddb66d6e27d987bb8210/"
               target="_blank"
               >合同会社わしら お問い合わせフォーム</a
-            >
+            ><br />
             ※ メール/電話でのお問い合わせは原則受け付けておりません。
           </td>
         </tr>
@@ -135,6 +152,15 @@
         </tr>
       </table>
     </div>
+  </div>
+
+  <div>
+    <h2>方針・規約 <small>[Policies]</small></h2>
+    <ul>
+      <li>
+        <a href="/policies/privacy-policy/">プライバシーポリシー</a>
+      </li>
+    </ul>
   </div>
 </template>
 
